562d5a751e24a188fce2bd8d3394ef011f30ff3d
TalibLyon <tzlyon@aggies.ncat.edu>
2026-02-13 14:27:14 -0500

    Add files via upload
    Uploading to new documents

---
+++ TalibLyon_assignment_4.py
@@ -0,0 +1,33 @@
+"""
+COMP 163 - Introduction to Programming
+Assignment: Chapter 4 - College Life Adventure Game
+Name: Talib Lyon
+GitHub Username: TalibLyon
+Date: 2/13/2026
+Description: This game simulates a semester at a prestigious school for sorcerers where the player must balance GPA, stress, study time, and social life while taking on missions and strengthening cursed skills.
+AI Usage: Used for debugging and clarification of assignment instructions.
+"""
+
+# ========================================
+# Initial Student Setup
+# ========================================
+
+student_name = "Talib"
+current_gpa = 3.2
+study_hours = 11
+social_points = 10
+stress_level = 43
+
+print("Welcome to College Life Adventure,", student_name + "!")
+print("Here are your starting stats:")
+print("GPA:", current_gpa)
+print("Study Hours:", study_hours)
+print("Social Points:", social_points)
+print("Stress Level:", stress_level)
+
+
+#test anything
+print("hello")
+
+
+
